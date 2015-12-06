6f4bc0e1752d142a2a5dad7bcba8f044ae8c9621
leafrontye <leafront@126.com>
2015-12-06 22:02:36 +0800

    modified js file localStorage.js file change module.exprots=strage mdoule

--- localstorage.js
+++ localstorage.js
@@ -1,63 +1,63 @@
 define(function(require,exports,module){
-        var UserData = {
-            userData : null,
-            name:location.hostname,
-            init:function(){
-                if (!UserData.userData) {
-                    try {
-                        UserData.userData = document.createElement('INPUT');
-                        UserData.userData.type = "hidden";
-                        UserData.userData.style.display = "none";
-                        UserData.userData.addBehavior ("#default#userData");
-                        document.body.appendChild(UserData.userData);
-                        var expires = new Date();
-                        expires.setDate(expires.getDate()+365);
-                        UserData.userData.expires = expires.toUTCString();
-                    } catch(e) {
-                        return false;
-                    }
+    var UserData = {
+        userData : null,
+        name:location.hostname,
+        init:function(){
+            if (!UserData.userData) {
+                try {
+                    UserData.userData = document.createElement('INPUT');
+                    UserData.userData.type = "hidden";
+                    UserData.userData.style.display = "none";
+                    UserData.userData.addBehavior ("#default#userData");
+                    document.body.appendChild(UserData.userData);
+                    var expires = new Date();
+                    expires.setDate(expires.getDate()+365);
+                    UserData.userData.expires = expires.toUTCString();
+                } catch(e) {
+                    return false;
                 }
-                return true;
-            },
-
-            setItem : function(key, value) {
-
-                if(UserData.init()){
-                    UserData.userData.load(UserData.name);
-                    UserData.userData.setAttribute(key, value);
-                    UserData.userData.save(UserData.name);
-                }
-            },
-
-            getItem : function(key) {
-                if(UserData.init()){
-                    UserData.userData.load(UserData.name);
-                    return UserData.userData.getAttribute(key)
-                }
-            },
-
-            remove : function(key) {
-                if(UserData.init()){
-                    UserData.userData.load(UserData.name);
-                    UserData.userData.removeAttribute(key);
-                    UserData.userData.save(UserData.name);
-                }
-
             }
-        };
-        var localStorage={};
-        localStorage.setItem=function(key,value){
+            return true;
+        },
+
+        setItem : function(key, value) {
+
+            if(UserData.init()){
+                UserData.userData.load(UserData.name);
+                UserData.userData.setAttribute(key, value);
+                UserData.userData.save(UserData.name);
+            }
+        },
+
+        getItem : function(key) {
+            if(UserData.init()){
+                UserData.userData.load(UserData.name);
+                return UserData.userData.getAttribute(key)
+            }
+        },
+
+        remove : function(key) {
+            if(UserData.init()){
+                UserData.userData.load(UserData.name);
+                UserData.userData.removeAttribute(key);
+                UserData.userData.save(UserData.name);
+            }
+
+        }
+    };
+    var storage={
+        setItem:function(key,value){
             if(!window.localStorage){
                 UserData.setItem(key,value);
             }else{
                 localStorage.setItem(key,value);
             }
         },
-        localStorage.getItem=function(key){
+        getItem:function(key){
             var value="";
             if(!window.localStorage){
                 if(UserData.getItem(key)==undefined) {
-                   //Do Nothing!
+                    //Do Nothing!
                 }else{
                     value=UserData.getItem(key);
                 }
@@ -70,13 +70,13 @@ define(function(require,exports,module){
             }
             return value;
         },
-        localStorage.remove=function(key){
+        remove:function(key){
             if(!window.localStorage){
                 UserData.remove(key);
             }else{
                 localStorage.removeItem(key);
             }
         }
-        module.exports=localStorage;
-});
-
+    };
+    module.exports=storage;
+})
